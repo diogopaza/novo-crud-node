90924a33701bc35eb0c67f8cbad0143b269c509a
Diogo Paza <diogopazacvel@gmail.com>
2017-12-06 12:18:11 -0200

    iniciando crud

---
+++ server.js
@@ -0,0 +1,12 @@
+/*
+    Arquivo: server.js
+    Descrição: 
+    Author: Diogo do Nascimento Paza
+
+*/
+
+//configurar setup da aplicação
+
+var express = require("express")
+var app = express()
+var bodyParser = require("body-parser")
